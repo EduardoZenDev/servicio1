b3a486458948c1bb297924b1b0f1861cae6a580f
EduardoZenDev <lalozen68@gmail.com>
2025-05-19 23:52:37 -0300

    Primer commit: proyecto Node.js sin node_modules y .env2

--- db/connection.js
+++ db/connection.js
@@ -3,11 +3,12 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
+
 
 connection.connect(err => {
   if (err) {
