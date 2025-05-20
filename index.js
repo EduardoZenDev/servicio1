0e6f9c3ef9fa229a20c3f0d1662e85cc952c7ef2
EduardoZenDev <lalozen68@gmail.com>
2025-05-20 00:15:08 -0300

    fix: agregar puerto para Render

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
+const express = require('express');
 const mysql = require('mysql2');
+const app = express();
+
+app.use(express.json()); // Para parsear JSON
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT), // asegúrate que sea número
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -17,4 +21,13 @@ connection.connect(error => {
     console.log('Conectado correctamente a la base de datos Railway');
   }
 });
-console.log('DB_HOST:', process.env.DB_HOST);
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.send('¡Servicio INE activo desde Render!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
